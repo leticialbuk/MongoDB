a00791d90a0847a0f72988ba2d702232071e373b
leticialbuk <leticia.albuk@gmail.com>
2023-08-17 19:12:57 -0300

    finalizando listagem de documentos

---
+++ MongoDB/MongoDB/ListandoDocsFiltroBson.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDB
+{
+    internal class ListandoDocsFiltroBson
+    {
+        static void Main(string[] args)
+        {
+            Task T = MainSync(args);
+            Console.WriteLine("Precione ENTER");
+            Console.ReadLine();
+        }
+
+        static async Task MainSync(string[] args)
+        {
+            var conexaoLivros = new DbContext();
+            Console.WriteLine("Documentos listados");
+        }
+    }
+}
